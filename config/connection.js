964c6def46a4f02cb301a120c4bd919b455ef00d
Kyle Woodruff <kylewoodruff@gmail.com>
2019-07-22 20:57:45 -0700

    Changes to connection variable

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,10 @@
 let mysql = require("mysql");
 
-const connection = "";
-
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 
-  connection = mysql.createConnection({
+  var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
